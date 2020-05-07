20ae96e07b687e1025e81942cb7f5cf5647af086
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-05-07 03:28:59 -0500

    just resume

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function show_more (d, a) {
-    if(document.getElementById(d).style.display == "none") {
-        document.getElementById(d).style.display = "block";
-        window.location = a;
-    } else {
-        document.getElementById(d).style.display = "none";
-    }
-}
